313ae1da1ec3b51a2f8610ba4f8dc47962905093
Tamagosushio <mitachi.ts75@gmail.com>
2025-05-29 12:18:52 +0900

    [update] 振り子クラスを改良

--- SushiGUI/Pendulum.cpp
+++ SushiGUI/Pendulum.cpp
@@ -5,17 +5,37 @@
 /// @param theta 初期角度 (初期値: pi/2)
 /// @param length 振り子の長さ (初期値: 1.0)
 /// @param gravity 重力加速度 (初期値: 9.8)
-SimplePendulum::SimplePendulum(double theta, double length, double gravity)
-  : theta0(theta), length0(length), g(gravity) {}
+SimplePendulum::SimplePendulum(
+  double theta, double length, double gravity,
+  double scale, Vec2 offset
+) : theta0(theta), length0(length), g(gravity), scale(scale), offset(offset) {}
 
 /// @brief 振り子を更新
 /// @return 振り子のx,y座標
-Vec2 SimplePendulum::update(void) {
+void SimplePendulum::update(void) {
   this->theta0_a = -g * Sin(theta0);
   this->theta0_v += theta0_a * Scene::DeltaTime();
   this->theta0 += theta0_v * Scene::DeltaTime();
-  Vec2 coordinate{ length0 * Sin(theta0), length0 * Cos(theta0) };
-  return coordinate;
+  this->coordinate = Vec2{
+    length0 * Sin(theta0),
+    length0 * Cos(theta0)
+  };
+}
+
+Vec2 SimplePendulum::get_coordinate(void) const {
+  return this->coordinate * scale + offset;
+}
+void SimplePendulum::set_scale(double scale) {
+  this->scale = scale;
+}
+double SimplePendulum::get_scale(void) const {
+  return this->scale;
+}
+void SimplePendulum::set_offset(const Vec2& offset) {
+  this->offset = offset;
+}
+Vec2 SimplePendulum::get_offset(void) const {
+  return this->offset;
 }
 
 /// @brief 二重振り子のパラメータの初期値
@@ -25,10 +45,11 @@ Vec2 SimplePendulum::update(void) {
 /// @param length1 1番目の振り子の長さ (初期値: 1.0)
 /// @param gravity 重力加速度 (初期値: 9.8) 
 DoublePendulum::DoublePendulum(
-    double theta0, double length0, double mass0,
-    double theta1, double length1, double mass1,
-    double gravity
-): SimplePendulum(theta0, length0, gravity), mass0(mass0), theta1(theta1), length1(length1), mass1(mass1) {}
+  double theta0, double length0, double mass0,
+  double theta1, double length1, double mass1,
+  double gravity,
+  double scale, Vec2 off_set  
+): SimplePendulum(theta0, length0, gravity, scale, off_set), mass0(mass0), theta1(theta1), length1(length1), mass1(mass1) {}
 void DoublePendulum::update(void) {
 
   double delta_time = Scene::DeltaTime();
@@ -55,9 +76,9 @@ void DoublePendulum::update(void) {
 
 
 Vec2 DoublePendulum::get_coordinate0(void)const {
-  return this->coordinate0;
+  return this->coordinate0 * scale + offset;
 }
 Vec2 DoublePendulum::get_coordinate1(void)const {
-  return this->coordinate1;
+  return this->coordinate1 * scale + offset;
 }
 
--- SushiGUI/Pendulum.hpp
+++ SushiGUI/Pendulum.hpp
@@ -8,18 +8,30 @@ protected:
   double theta0 = 0.5_pi; // 角度の初期値
   double theta0_v = 0.0; // 角速度
   double theta0_a = 0.0; // 角加速度
+  Vec2 coordinate{ 0.0, 0.0 }; // 振り子のx,y座標
+  double scale = 1.0; // 振り子のスケール
+  Vec2 offset{ 0.0, 0.0 }; // 振り子のオフセット座標
 public:
   // 振り子パラメターの初期化
-  SimplePendulum(double theta = 0.5_pi, double length = 1.0, double gravity = 9.8);
+  SimplePendulum(
+    double theta = 0.5_pi, double length = 1.0, double gravity = 9.8,
+    double scale = 1.0, Vec2 offset = Vec2{ 0.0, 0.0 }
+  );
   // 振り子を更新
-  Vec2 update(void);
+  void update(void);
+  // 振り子のx,y座標を取得
+  Vec2 get_coordinate(void) const;
+  void set_scale(double scale);
+  double get_scale(void) const;
+  void set_offset(const Vec2& offset);
+  Vec2 get_offset(void) const;
 };
 
 /// @brief 二重振り子のシミュレーション
-class DoublePendulum : protected SimplePendulum {
+class DoublePendulum : public SimplePendulum {
 protected:
-  const double mass0 = 1.0;
-  const double mass1 = 1.0;
+  const double mass0 = 1.0; // 0番目の振り子の質量
+  const double mass1 = 1.0; // 1番目の振り子の質量
   const double length1 = 1.0;
   double theta1 = 0.5_pi;
   double theta1_v = 0.0;
@@ -30,9 +42,13 @@ public:
   DoublePendulum(
     double theta0 = 0.5_pi, double length0 = 1.0, double mass0 = 1.0,
     double theta1 = 0.5_pi, double length1 = 1.0, double mass1 = 1.0,
-    double gravity = 9.8
+    double gravity = 9.8,
+    double scale = 1.0, Vec2 off_set = Vec2{ 0.0, 0.0 }
   );
+  // 振り子を更新
   void update(void);
+  // 0番目の振り子のx,y座標を取得
   Vec2 get_coordinate0(void) const;
+  // 1番目の振り子のx,y座標を取得
   Vec2 get_coordinate1(void) const;
 };
