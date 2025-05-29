fe44c4781da94ad41244569bdb65bfded04543b8
Tamagosushio <mitachi.ts75@gmail.com>
2025-05-29 11:29:06 +0900

    [fix] typo修正

--- SushiGUI/Main.cpp
+++ SushiGUI/Main.cpp
@@ -13,7 +13,7 @@ void Main() {
   //SimplePendulum pendulum3{ 0.75_pi, 2.0, 9.8*2 };
   //SimplePendulum pendulum4{ 0.50_pi, 1.0, 9.8*2 };
 
-  DoublePendulum d_penulum{
+  DoublePendulum d_pendulum{
     0.50_pi, 1.5, 2.0,
     0.00_pi, 0.5, 0.5
   };
@@ -28,16 +28,16 @@ void Main() {
     //Circle{ Scene::CenterF() + 100 * pendulum3.update(), 5.0 }.draw(Palette::Blue);
     //Circle{ Scene::CenterF() + 100 * pendulum4.update(), 5.0 }.draw(Palette::Black);
 
-    d_penulum.update();
+    d_pendulum.update();
 
 
-    Circle{ Scene::CenterF() + 100 * d_penulum.get_coordinate0(), 5.0}.draw(Palette::Black);
-    Circle{ Scene::CenterF() + 100 * d_penulum.get_coordinate1(), 5.0 }.draw(Palette::Black);
+    Circle{ Scene::CenterF() + 100 * d_pendulum.get_coordinate0(), 5.0}.draw(Palette::Black);
+    Circle{ Scene::CenterF() + 100 * d_pendulum.get_coordinate1(), 5.0 }.draw(Palette::Black);
 
-    Line{ Scene::CenterF(), Scene::CenterF() + 100 * d_penulum.get_coordinate0() }.draw(Palette::Green);
-    Line{ Scene::CenterF() + 100 * d_penulum.get_coordinate0(), Scene::CenterF() + 100 * d_penulum.get_coordinate1() }.draw(Palette::Green);
+    Line{ Scene::CenterF(), Scene::CenterF() + 100 * d_pendulum.get_coordinate0() }.draw(Palette::Green);
+    Line{ Scene::CenterF() + 100 * d_pendulum.get_coordinate0(), Scene::CenterF() + 100 * d_pendulum.get_coordinate1() }.draw(Palette::Green);
 
-    points << Scene::CenterF() + 100 * d_penulum.get_coordinate1();
+    points << Scene::CenterF() + 100 * d_pendulum.get_coordinate1();
 
     for (const Vec2& point : points) {
       Circle{ point, 1.0 }.draw(Palette::Red);
